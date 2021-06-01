e79dd52c0cc5dd3c848a4c40053cc7a75697a78f
Karl Anthony Arnejo <karlanthonyarnejo@gmail.com>
2021-06-01 10:51:08 +0800

    initial push

---
+++ src/main/java/com/vertere_demo/vertere_demo/VertereDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vertere_demo.vertere_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertereDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertereDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vertere_demo/vertere_demo/VertereDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertere_demo.vertere_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertereDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
